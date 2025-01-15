9c0116382253e9f4c180a7ac8f251ff9d900251c
ssssstella <wt.su77@gmail.com>
2025-01-14 16:42:19 -0800

    fix default cells

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -12,6 +12,48 @@ interface LocalApiError {
   code: string;
 }
 
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 7);
+}
+
+const defaultIds = [randomId(), randomId()];
+const defaultContent = [
+  `# JBook
+  
+  This is an interactive Javascript coding environment. You can write Javascript, see it executed, and write comprehensive documentation using markdown.
+  
+  - Click any cell (including this one) to edit it
+  - The code in each code editor cell is all joined together into one file. If you define a variable in cell #1, you can refer to it in any following cell!
+  - You can show any React component, string, number, or anything else by calling the \`show\` function. This is a built-in function in this environment. Call show multiple times to show multiple values
+  - Re-order or delete cells using the buttons on the top right of the cell
+  - Add new cells by hovering on the divider between each cell
+  All of your changes get saved to the file you opened JBook with. So if you ran \`npx jbook serve test.js\`, all of the text and code you write will be saved to the \`test.js\` file.  
+  `, 
+  
+  `import {useState} from 'react';
+    const Counter = () => {
+      const [count, setCount] = useState(0);
+      return (
+        <div>
+          <button onClick={() => setCount(count + 1)}>Click</button>
+          <h3>Count: {count}</h3>
+        </div>
+      )
+    };
+  
+  // Display any variable or React Component by calling 'show'
+  show(<Counter />);
+  `
+  ];
+
+const defaultCells = defaultIds.map((key, index) => {
+  return {
+    id: key,
+    type: index % 2 === 0 ? 'text' : 'code',
+    content: defaultContent[index]
+  };
+});
+
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
   router.use(express.json());
@@ -30,8 +72,8 @@ export const createCellsRouter = (filename: string, dir: string) => {
     } catch (err) {
       if (isLocalApiError(err)) {
         if (err.code === 'ENOENT') {
-          await fs.writeFile(fullPath, '[]', 'utf-8');
-          res.send([]);
+          await fs.writeFile(fullPath, JSON.stringify(defaultCells), 'utf-8');
+          res.send(defaultCells);
         }
       } else {
         throw err;
